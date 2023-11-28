e41f514b9a8e9a05db396e1f23f79d3a674092e1
ThanasisGeorg <thanasisgeorg03@gmail.com>
2023-11-28 21:31:03 +0200

    get gnome extensions done

--- src/apt_methods.py
+++ src/apt_methods.py
@@ -1,11 +1,14 @@
 import subprocess
 
-
 def get_apt_packages():
     result = subprocess.run(['apt', 'list', '--installed'], stdout=subprocess.PIPE)
     packages = str(result.stdout).replace("\'", "").strip()
     return packages.split("\\n")
 
+def get_gnome_extensions():
+    result = subprocess.run(['gnome-extensions', 'list'], stdout=subprocess.PIPE)
+    result = str(result.stdout).replace("b'", "")
+    return result.split("\\n")
 
 def install_apt_packages(packages):
     for line in packages:
--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 import apt_methods
 
-for line in apt_methods.get_apt_packages():
+#for line in apt_methods.get_apt_packages():
+    #print(line)
+    
+for line in apt_methods.get_gnome_extensions():
     print(line)
 
