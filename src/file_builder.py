f3e1bb0cac0897a5ed0f8be88c55b90580fef065
Konstantinos Despoinidis <kdesp2003@gmail.com>
2023-12-13 19:47:41 +0200

    cleaned file builder class

--- src/file_builder.py
+++ src/file_builder.py
@@ -74,16 +74,16 @@ class FileBuilder:
 
         contents += "\n\n\n"
 
-        contents += system_settings() if self.system_settings_included else ""
-        contents += apt_packages() if self.apt_packages_included else ""
-        contents += snap_packages() if self.snap_packages_included else ""
-        contents += flatpak_packages() if self.flatpak_packages_included else ""
-        contents += gnome_extensions() if self.gnome_extensions_included else ""
-        contents += repository_keys() if self.repository_keys_included else ""
-        contents += shell_themes() if self.shell_themes_included else ""
-        contents += apt_repositories() if self.apt_repositories_included else ""
-        contents += ssh() if self.ssh_included else ""
-        contents += configs() if self.configs_included else ""
+        contents += self.__system_settings() if self.system_settings_included else ""
+        contents += self.__apt_packages() if self.apt_packages_included else ""
+        contents += self.__snap_packages() if self.snap_packages_included else ""
+        contents += self.__flatpak_packages() if self.flatpak_packages_included else ""
+        contents += self.__gnome_extensions() if self.gnome_extensions_included else ""
+        contents += self.__repository_keys() if self.repository_keys_included else ""
+        contents += self.__shell_themes() if self.shell_themes_included else ""
+        contents += self.__apt_repositories() if self.apt_repositories_included else ""
+        contents += self.__ssh() if self.ssh_included else ""
+        contents += self.__configs() if self.configs_included else ""
 
         file = open(filename, "w")
         file.write(contents)
@@ -91,87 +91,77 @@ class FileBuilder:
 
         print("File written successfully")
 
+    def __system_settings(self):
+        contents = constants.SYSTEM_SETTINGS + "\n"
+        contents += constants.SEPARATOR + "\n\n\n"
+        return contents
 
-def system_settings():
-    contents = constants.SYSTEM_SETTINGS + "\n"
-    contents += constants.SEPARATOR + "\n\n\n"
-    return contents
+    def __apt_packages(self):
+        contents = constants.APT_PACKAGES + "\n"
+        for package in get_methods.get_apt_packages():
+            contents += package + "\n"
+        contents += constants.SEPARATOR + "\n\n\n"
+        return contents
 
+    def __snap_packages(self):
+        contents = constants.SNAP_PACKAGES + "\n"
+        for package in get_methods.get_snap_packages():
+            contents += package + "\n"
+        contents += constants.SEPARATOR + "\n\n\n"
+        return contents
 
-def apt_packages():
-    contents = constants.APT_PACKAGES + "\n"
-    for package in get_methods.get_apt_packages():
-        contents += package + "\n"
-    contents += constants.SEPARATOR + "\n\n\n"
-    return contents
+    def __flatpak_packages(self):
+        contents = constants.FLATPAK_PACKAGES + "\n"
+        for package in get_methods.get_flatpak_packages():
+            contents += package + "\n"
+        contents += constants.SEPARATOR + "\n\n\n"
+        return contents
 
+    def __gnome_extensions(self):
+        contents = constants.GNOME_EXTENSIONS + "\n"
+        for extension in get_methods.get_gnome_extensions():
+            contents += extension + "\n"
+        contents += constants.SEPARATOR + "\n\n\n"
+        return contents
 
-def snap_packages():
-    contents = constants.SNAP_PACKAGES + "\n"
-    for package in get_methods.get_snap_packages():
-        contents += package + "\n"
-    contents += constants.SEPARATOR + "\n\n\n"
-    return contents
-
-
-def flatpak_packages():
-    contents = constants.FLATPAK_PACKAGES + "\n"
-    for package in get_methods.get_flatpak_packages():
-        contents += package + "\n"
-    contents += constants.SEPARATOR + "\n\n\n"
-    return contents
-
-
-def gnome_extensions():
-    contents = constants.GNOME_EXTENSIONS + "\n"
-    for extension in get_methods.get_gnome_extensions():
-        contents += extension + "\n"
-    contents += constants.SEPARATOR + "\n\n\n"
-    return contents
-
-
-def repository_keys():
-    contents = constants.REPOSITORY_KEYS + "\n"
-    for pair in get_methods.get_sources_keys():
-        contents += pair[0] + "\n"
-        contents += str(pair[1]) + "\n\n"
-    contents += constants.SEPARATOR + "\n\n\n"
-    return contents
-
-
-def shell_themes():
-    contents = constants.SHELL_THEMES + "\n"
-    try:
-        contents += str(get_methods.get_shell_themes())
-    except FileNotFoundError:
-        contents += "Error\n"
-        print("Error with shell themes")
-    contents += constants.SEPARATOR + "\n\n\n"
-    return contents
-
-
-def apt_repositories():
-    contents = constants.APT_REPOSITORIES + "\n"
-    for repo in get_methods.get_apt_repos():
-        contents += repo + "\n"
-    contents += constants.SEPARATOR + "\n\n\n"
-    return contents
-
-
-def ssh():
-    contents = constants.SSH + "\n"
-    try:
-        for pair in get_methods.get_ssh_keys():
+    def __repository_keys(self):
+        contents = constants.REPOSITORY_KEYS + "\n"
+        for pair in get_methods.get_sources_keys():
             contents += pair[0] + "\n"
-            contents += pair[1] + "\n\n"
-    except FileNotFoundError:
-        print("Error with ssh")
-        contents += "Error\n"
-    contents += constants.SEPARATOR + "\n\n\n"
-    return contents
+            contents += str(pair[1]) + "\n\n"
+        contents += constants.SEPARATOR + "\n\n\n"
+        return contents
 
+    def __shell_themes(self):
+        contents = constants.SHELL_THEMES + "\n"
+        try:
+            contents += str(get_methods.get_shell_themes())
+        except FileNotFoundError:
+            contents += "Error\n"
+            print("Error with shell themes")
+        contents += constants.SEPARATOR + "\n\n\n"
+        return contents
 
-def configs():
-    contents = constants.CONFIGS + "\n"
-    contents += constants.SEPARATOR + "\n\n\n"
-    return contents
+    def __apt_repositories(self):
+        contents = constants.APT_REPOSITORIES + "\n"
+        for repo in get_methods.get_apt_repos():
+            contents += repo + "\n"
+        contents += constants.SEPARATOR + "\n\n\n"
+        return contents
+
+    def __ssh(self):
+        contents = constants.SSH + "\n"
+        try:
+            for pair in get_methods.get_ssh_keys():
+                contents += pair[0] + "\n"
+                contents += pair[1] + "\n\n"
+        except FileNotFoundError:
+            print("Error with ssh")
+            contents += "Error\n"
+        contents += constants.SEPARATOR + "\n\n\n"
+        return contents
+
+    def __configs(self):
+        contents = constants.CONFIGS + "\n"
+        contents += constants.SEPARATOR + "\n\n\n"
+        return contents
