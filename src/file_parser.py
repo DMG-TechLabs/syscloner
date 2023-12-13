9db3e26c280a473da4683d4834c0c34e9642e1f9
Konstantinos Despoinidis <kdesp2003@gmail.com>
2023-12-13 17:54:13 +0200

    made some fields of FileParser private

--- src/file_parser.py
+++ src/file_parser.py
@@ -2,96 +2,97 @@ import constants
 
 
 class FileParser:
-    index = 0
-    lines = []
+    # Private
+    __index = 0
+    __lines = []
 
     system_settings = b""
+    shell_themes = b""
     apt_packages = []
     snap_packages = []
     flatpak_packages = []
     gnome_extensions = []
     apt_repositories = ""
     repository_keys = list()    # 2d array
-    shell_themes = b""
     ssh = list()  # 2d array
     configs = list()    # 2d array
 
     def __init__(self, file):
         self.file = open(file, "r")
-        try: 
-            self.lines = self.file.read().split("\n")
+        try:
+            self.__lines = self.file.read().split("\n")
         except FileNotFoundError:
             print("File not found. Aborting parsing...\n")
 
     def advance(self):
-        self.index += 1
+        self.__index += 1
 
     def parse(self):
-        while self.index < self.lines.__len__():
-            if self.lines[self.index] == constants.SYSTEM_SETTINGS:
+        while self.__index < self.__lines.__len__():
+            if self.__lines[self.__index] == constants.SYSTEM_SETTINGS:
                 self.advance()
-                self.system_settings = self.lines[self.index]
+                self.system_settings = self.__lines[self.__index]
                 self.advance()  # Consume closing tag
-            elif self.lines[self.index] == constants.SHELL_THEMES:
+            elif self.__lines[self.__index] == constants.SHELL_THEMES:
                 self.advance()
-                self.shell_themes = self.lines[self.index]
+                self.shell_themes = self.__lines[self.__index]
                 self.advance()  # Consume closing tag
-            elif self.lines[self.index] == constants.APT_PACKAGES:
+            elif self.__lines[self.__index] == constants.APT_PACKAGES:
                 self.advance()
-                while self.lines[self.index] != constants.SEPARATOR:
-                    self.apt_packages.append(self.lines[self.index])
+                while self.__lines[self.__index] != constants.SEPARATOR:
+                    self.apt_packages.append(self.__lines[self.__index])
                     self.advance()
-            elif self.lines[self.index] == constants.SNAP_PACKAGES:
+            elif self.__lines[self.__index] == constants.SNAP_PACKAGES:
                 self.advance()
-                while self.lines[self.index] != constants.SEPARATOR:
-                    self.snap_packages.append(self.lines[self.index])
+                while self.__lines[self.__index] != constants.SEPARATOR:
+                    self.snap_packages.append(self.__lines[self.__index])
                     self.advance()
-            elif self.lines[self.index] == constants.FLATPAK_PACKAGES:
+            elif self.__lines[self.__index] == constants.FLATPAK_PACKAGES:
                 self.advance()
-                while self.lines[self.index] != constants.SEPARATOR:
-                    self.flatpak_packages.append(self.lines[self.index])
+                while self.__lines[self.__index] != constants.SEPARATOR:
+                    self.flatpak_packages.append(self.__lines[self.__index])
                     self.advance()
-            elif self.lines[self.index] == constants.GNOME_EXTENSIONS:
+            elif self.__lines[self.__index] == constants.GNOME_EXTENSIONS:
                 self.advance()
-                while self.lines[self.index] != constants.SEPARATOR:
-                    self.gnome_extensions.append(self.lines[self.index])
+                while self.__lines[self.__index] != constants.SEPARATOR:
+                    self.gnome_extensions.append(self.__lines[self.__index])
                     self.advance()
-            elif self.lines[self.index] == constants.APT_REPOSITORIES:
+            elif self.__lines[self.__index] == constants.APT_REPOSITORIES:
                 self.advance()
-                while self.lines[self.index] != constants.SEPARATOR:
-                    self.apt_repositories += self.lines[self.index] + "\n"
+                while self.__lines[self.__index] != constants.SEPARATOR:
+                    self.apt_repositories += self.__lines[self.__index] + "\n"
                     self.advance()
-            elif self.lines[self.index] == constants.REPOSITORY_KEYS:
+            elif self.__lines[self.__index] == constants.REPOSITORY_KEYS:
                 self.advance()
-                while self.lines[self.index] != constants.SEPARATOR:
-                    key_path = self.lines[self.index]
+                while self.__lines[self.__index] != constants.SEPARATOR:
+                    key_path = self.__lines[self.__index]
                     self.advance()
-                    key_bytes = self.lines[self.index]
+                    key_bytes = self.__lines[self.__index]
                     self.repository_keys.append([key_path, key_bytes])
                     self.advance()
 
-                    if self.lines[self.index] == "":
+                    if self.__lines[self.__index] == "":
                         self.advance()  # Consume blank line
-            elif self.lines[self.index] == constants.SSH:
+            elif self.__lines[self.__index] == constants.SSH:
                 self.advance()
-                while self.lines[self.index] != constants.SEPARATOR:
-                    key_path = self.lines[self.index]
+                while self.__lines[self.__index] != constants.SEPARATOR:
+                    key_path = self.__lines[self.__index]
                     self.advance()
-                    key_bytes = self.lines[self.index]
+                    key_bytes = self.__lines[self.__index]
                     self.ssh.append([key_path, key_bytes])
                     self.advance()
 
-                    if self.lines[self.index] == "":
+                    if self.__lines[self.__index] == "":
                         self.advance()  # Consume blank line
-            elif self.lines[self.index] == constants.CONFIGS:
+            elif self.__lines[self.__index] == constants.CONFIGS:
                 self.advance()
-                while self.lines[self.index] != constants.SEPARATOR:
-                    key_path = self.lines[self.index]
+                while self.__lines[self.__index] != constants.SEPARATOR:
+                    key_path = self.__lines[self.__index]
                     self.advance()
-                    key_bytes = self.lines[self.index]
+                    key_bytes = self.__lines[self.__index]
                     self.configs.append([key_path, key_bytes])
                     self.advance()
 
-                    if self.lines[self.index] == "":
+                    if self.__lines[self.__index] == "":
                         self.advance()  # Consume blank line
             self.advance()
